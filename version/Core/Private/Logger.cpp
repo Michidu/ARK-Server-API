2bf28b414ab10259d727d1f5478951f4c4e3a039
Michidu <michido@protonmail.com>
2018-01-27 14:01:30 +0300

    Added date and process id to the log files

--- version/Core/Private/Logger.cpp
+++ version/Core/Private/Logger.cpp
@@ -5,8 +5,10 @@ std::vector<spdlog::sink_ptr>& GetLogSinks()
 {
 	static std::vector<spdlog::sink_ptr> sinks{
 		std::make_shared<spdlog::sinks::wincolor_stdout_sink_mt>(),
-		std::make_shared<spdlog::sinks::rotating_file_sink_mt>(ArkApi::Tools::GetCurrentDir() + "/logs/ArkApi.log",
-		                                                       1024 * 1024, 5)
+		std::make_shared<spdlog::sinks::rotating_file_sink_mt>(
+			spdlog::sinks::default_daily_file_name_calculator::
+			calc_filename(ArkApi::Tools::GetCurrentDir() + "/logs/ArkApi_" + std::to_string(GetCurrentProcessId()) + ".log"),
+			1024 * 1024, 5)
 	};
 
 	return sinks;
