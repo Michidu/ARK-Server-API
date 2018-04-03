3957bd54a5af8cc1bda9e8cd2e9c144e233751f8
Michidu <michido@protonmail.com>
2018-04-03 10:11:30 +0300

    Requests fix

---
+++ version/Core/Private/Tools/Requests.cpp
@@ -0,0 +1,61 @@
+#include <Requests.h>
+
+namespace ArkApi
+{
+	Requests::Requests()
+	{
+		GetCommands().AddOnTimerCallback("RequestsUpdate", &Update);
+	}
+
+	Requests::~Requests()
+	{
+		GetCommands().RemoveOnTimerCallback("RequestsUpdate");
+	}
+
+	Requests& Requests::Get()
+	{
+		static Requests instance;
+		return instance;
+	}
+
+	bool Requests::CreateRequest(FString& url, FString& verb,
+	                             const std::function<void(TSharedRef<IHttpRequest>, bool)>& callback)
+	{
+		TSharedRef<IHttpRequest> request;
+		FHttpModule::Get()->CreateRequest(&request);
+
+		FString header_name = "Content-Type";
+		FString header_value = "text/html";
+
+		request->SetHeader(&header_name, &header_value);
+		request->SetURL(&url);
+		request->SetVerb(&verb);
+
+		requests_.Add({request, callback});
+
+		return request->ProcessRequest();
+	}
+
+	void Requests::Update()
+	{
+		auto& requests = Get().requests_;
+		for (int i = 0; i < requests.Num(); ++i)
+		{
+			auto request = requests[i];
+
+			const auto status = request.request->GetStatus();
+			switch (status)
+			{
+			case EHttpRequestStatus::Succeeded:
+			case EHttpRequestStatus::Failed:
+				requests.RemoveAt(i);
+				request.callback(request.request, status == EHttpRequestStatus::Succeeded);
+				break;
+			case EHttpRequestStatus::NotStarted:
+			case EHttpRequestStatus::Processing:
+				break;
+			default: ;
+			}
+		}
+	}
+}
---
+++ version/Core/Private/Tools/Tools.cpp
@@ -0,0 +1,80 @@
+#include <Tools.h>
+
+#include "../PluginManager/PluginManager.h"
+
+namespace ArkApi::Tools
+{
+	std::string GetCurrentDir()
+	{
+		char buffer[MAX_PATH];
+		GetModuleFileNameA(nullptr, buffer, MAX_PATH);
+
+		const std::string::size_type pos = std::string(buffer).find_last_of("\\/");
+
+		return std::string(buffer).substr(0, pos);
+	}
+
+	std::wstring ConvertToWideStr(const std::string& text)
+	{
+		const size_t size = text.size();
+
+		std::wstring wstr;
+		if (size > 0)
+		{
+			wstr.resize(size);
+
+			size_t converted_chars;
+			mbstowcs_s(&converted_chars, wstr.data(), size + 1, text.c_str(), _TRUNCATE);
+		}
+
+		return wstr;
+	}
+
+	std::string ConvertToAnsiStr(const std::wstring& text)
+	{
+		const size_t length = text.size();
+
+		std::string str(length, '\0');
+		std::use_facet<std::ctype<wchar_t>>(std::locale()).narrow(text.c_str(), text.c_str() + length, '?', str.data());
+
+		return str;
+	}
+
+	std::string Utf8Encode(const std::wstring& wstr)
+	{
+		if (wstr.empty())
+			return std::string();
+
+		const int size_needed = WideCharToMultiByte(CP_UTF8, 0, wstr.data(), static_cast<int>(wstr.size()), nullptr, 0,
+		                                            nullptr, nullptr);
+
+		std::string str(size_needed, 0);
+		WideCharToMultiByte(CP_UTF8, 0, wstr.data(), static_cast<int>(wstr.size()), str.data(), size_needed, nullptr,
+		                    nullptr);
+
+		return str;
+	}
+
+	std::wstring Utf8Decode(const std::string& str)
+	{
+		if (str.empty())
+			return std::wstring();
+
+		const int size_needed = MultiByteToWideChar(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), nullptr, 0);
+
+		std::wstring wstr(size_needed, 0);
+		MultiByteToWideChar(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), wstr.data(), size_needed);
+
+		return wstr;
+	}
+
+	bool IsPluginLoaded(const std::string& plugin_name)
+	{
+		return PluginManager::Get().IsPluginLoaded(plugin_name);
+	}
+
+	std::string GetApiVer()
+	{
+		return API_VERSION;
+	}
+}
