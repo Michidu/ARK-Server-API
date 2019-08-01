8893cf89663e9a28d55a07530fa8572fd0667b96
Michidu <michidu@protonmail.com>
2019-08-01 12:50:31 +0300

    .

--- version/Core/Private/Tools/Timer.cpp
+++ version/Core/Private/Tools/Timer.cpp
@@ -20,52 +20,28 @@ namespace API
 		return instance;
 	}
 
-	void Timer::DelayExecuteInternal(std::string timer_name, const std::function<void()>& callback, int delay_seconds)
+	void Timer::DelayExecuteInternal(const std::function<void()>& callback, int delay_seconds)
 	{
 		const auto now = std::chrono::system_clock::now();
 		const auto exec_time = now + std::chrono::seconds(delay_seconds);
 
-		timer_funcs_.emplace_back(std::make_unique<TimerFunc>(timer_name, exec_time, callback, true, 1, 0));
+		timer_funcs_.emplace_back(std::make_unique<TimerFunc>(exec_time, callback, true, 1, 0));
 	}
 
-	bool Timer::IsTimerRunningInternal(std::string timer_name)
-	{
-		const auto& timer_funcs = std::find_if(timer_funcs_.begin(), timer_funcs_.end(), [&timer_name](const auto& data)
-		{
-			return timer_id == data->timer_id;
-		});
-
-		if (timer_funcs != timer_funcs_.end())
-			return true;
-
-		const auto& timer_thread = std::find_if(timer_threads_.begin(), timer_threads_.end(), [&timer_name](const auto& data)
-		{
-			return timer_id == data->timer_id;
-		});
-
-		return timer_thread != timer_threads_.end() && timer_thread->get()->alive;
-	}
-
-	void Timer::RecurringExecuteInternal(std::string timer_name, const std::function<void()>& callback,
-										 int execution_interval, int execution_counter, bool async)
+	void Timer::RecurringExecuteInternal(const std::function<void()>& callback, int execution_interval,
+	                                     int execution_counter, bool async)
 	{
 		if (async)
 		{
-			timer_threads_.emplace_back(std::make_unique<TimerThread>(timer_name,
-				std::thread([timer_name, callback, execution_interval, execution_counter, this]()
+			std::thread([callback, execution_interval, execution_counter]()
 			{
 				if (execution_counter == -1)
 				{
-					for (; Timer::Get().IsTimerRunning(timer_name);)
+					for (;;)
 					{
 						callback();
 						std::this_thread::sleep_for(std::chrono::seconds(execution_interval));
-					}		
-					
-					timer_threads_.erase(std::remove_if(timer_threads_.begin(), timer_threads_.end(), [&timer_name](const auto& data)
-					{
-						return timer_id == data->timer_id;
-					}), timer_threads_.end());
+					}
 				}
 
 				for (int i = 0; i < execution_counter; ++i)
